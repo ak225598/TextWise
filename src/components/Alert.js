cc3e99e42c8d9e0023c80a4d9f1824b652c7db4c
Ankit <ankit.iiitbh@gmail.com>
2024-01-18 18:09:36 +0530

    Resized alert popup message box

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,10 +1,11 @@
 import React from 'react'
 function Alert(props) {
     return (
-            props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+        <div className="d-flex justify-content-end my-2 mx-5" style={{height : '30px'}}>
+            {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show bg-success text-white mx-4 my-0 p-1 fs-6`} role="alert">
                 <strong>{props.alert.type}</strong> : {props.alert.msg}
-            </div>
-
+            </div>}
+        </div>
     )
 }
 
